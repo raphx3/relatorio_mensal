36577d5966b701cea7c7a5da760756d8334b5bbd
raphx3 <raphaelalvarengadasilva@gmail.com>
2025-03-19 18:04:26 +0000

    Update GERAR_RELATORIO_PD_METEO.py

--- GERAR_RELATORIO_PD_METEO.py
+++ GERAR_RELATORIO_PD_METEO.py
@@ -1,85 +1,56 @@
+import os
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime as dtt
-import os
+import io
+import streamlit as st
 
-# Função para processar colunas e extrair dados numéricos
-def processar_colunas_amplitude_speed(df_filtrado):
-    amplitude_cols = [col for col in df_filtrado.columns if "Amplitude" in col and "Flag_" not in col  and "GMT-03:00" not in col]
-    speed_cols = [col for col in df_filtrado.columns if "Speed" in col and "Flag_" not in col  and "GMT-03:00" not in col]
+# Função para exibir a página no Streamlit
+def exibir_pagina_streamlit():
+    st.title("Relatório de Séries Temporais")
     
-    df_amplitude = df_filtrado[amplitude_cols].apply(pd.to_numeric, errors='coerce')  
-    df_speed = df_filtrado[speed_cols].apply(pd.to_numeric, errors='coerce')  
+    # Exibição de campos de entrada no Streamlit
+    pasta_saida = st.text_input(
+        'Insira o caminho da pasta onde os resultados serão salvos:', 
+        r'C:\Users\Rafael Alvarenga UMI\Desktop\PD_METEO\REPORTES\RELATORIO_MENSAL\RESULTADOS',
+        key='input_pasta_saida'
+    )
+    
+    # Botão para gerar os gráficos
+    if st.button('Gerar Gráficos'):
+        # A seguir, chamaremos a função plot_series_temporais
+        df_filtrado = pd.DataFrame()  # Suponha que você tenha um DataFrame já carregado
+        parameter_columns = ['Amplitude', 'Speed', 'Temperature']  # Exemplos de parâmetros
+        parametro_para_teste = 'Exemplo de Teste'
+        
+        plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste, pasta_saida)
 
-    return df_amplitude, df_speed, amplitude_cols, speed_cols
+# Função para filtrar dados por período (se necessário, você pode adaptá-la)
+def filtrar_por_periodo(df, inicio, fim):
+    return df[(df['GMT-03:00'] >= inicio) & (df['GMT-03:00'] <= fim)]
 
-# Função para calcular limites do gráfico
-def calcular_limites(df_amplitude, df_speed):
+# Função para gerar os gráficos das séries temporais
+def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste, pasta_saida):
+    agora = dtt.datetime.now()
+    agora = agora.strftime('%Y/%m/%d %H:%M:%S')
+
+    # Encontrar as colunas que possuem "Amplitude" no nome (excluindo "Flag_")
+    amplitude_cols = [col for col in df_filtrado.columns if "Amplitude" in col and "Flag_" not in col and "GMT-03:00" not in col]
+    speed_cols = [col for col in df_filtrado.columns if "Speed" in col and "Flag_" not in col and "GMT-03:00" not in col]
+    
+    # Garantir que as colunas de "Amplitude" sejam numéricas, descartando valores não numéricos
+    df_amplitude = df_filtrado[amplitude_cols].apply(pd.to_numeric, errors='coerce')
+    df_speed = df_filtrado[speed_cols].apply(pd.to_numeric, errors='coerce')
+    
+    # Calcular os limites globais do eixo Y com base nas colunas "Amplitude"
     if not df_amplitude.empty:
         ymin_amplitude = df_amplitude.min().min() * 0.7
         ymax_amplitude = df_amplitude.max().max() * 1.3
-    else:
-        ymin_amplitude, ymax_amplitude = None, None
-
+    
     if not df_speed.empty:
         ymin_speed = df_speed.max().max() * -0.1
         ymax_speed = df_speed.max().max() * 1.1
-    else:
-        ymin_speed, ymax_speed = None, None
-
-    return ymin_amplitude, ymax_amplitude, ymin_speed, ymax_speed
-
-# Função para configurar os rótulos do eixo Y de acordo com o parâmetro
-def configurar_rotulos(param):
-    rotulos = {
-        "Amplitude": "Amplitude (m)",
-        "Speed": "Velocidade (m/s)",
-        "Direction": "Direção (°)",
-        "Roll": "Direção (°)",
-        "Heading": "Direção (°)",
-        "Pitch": "Direção (°)",
-        "Temperature": "Temperatura (°C)",
-        "Pressure": "Pressão (dbar)",
-        "Battery": "Tensão (Volts)",
-        "Rain": "Altura (mm)",
-        "Wind Direction(*)": "Direção (°)",
-        "Gust Speed(m/s)": "Velocidade (m/s)",
-        "Wind Speed(m/s)": "Velocidade (m/s)",
-        "Dew Point": "Temperatura (°C)",
-        "RH(%)": "Umidade (%)",
-        "Tide_Level": "Altura (m)",
-        "Sensor_Velki": "Altura (m)",
-        "CutOff_Freq_High": "Frequência (n/s)",
-        "Peak_Period": "Período (s)",
-        "Max_Height": "Altura (m)",
-        "Sign_Height": "Altura (m)"
-    }
-
-    for chave in rotulos:
-        if chave in param:
-            return rotulos[chave]
-    return "Valor"
-
-# Função para garantir que o eixo X tenha 5 ticks igualmente espaçados
-def configurar_ticks_x(df_filtrado):
-    time_min = df_filtrado['GMT-03:00'].min()
-    time_max = df_filtrado['GMT-03:00'].max()
-    time_min_timestamp = time_min.timestamp()
-    time_max_timestamp = time_max.timestamp()
-
-    xticks_positions_timestamp = np.linspace(time_min_timestamp, time_max_timestamp, 5)
-    xticks_positions = [pd.to_datetime(ts, unit='s') for ts in xticks_positions_timestamp]
-
-    return xticks_positions
-
-#%% plot_series_temporais
-def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste, pasta_saida):  
-    agora = dtt.datetime.now()
-    agora = agora.strftime('%Y/%m/%d %H:%M:%S')
-    
-    df_amplitude, df_speed, amplitude_cols, speed_cols = processar_colunas_amplitude_speed(df_filtrado)
-    ymin_amplitude, ymax_amplitude, ymin_speed, ymax_speed = calcular_limites(df_amplitude, df_speed)
 
     for param in parameter_columns:
         flag_column = f'Flag_{param}'
@@ -88,6 +59,7 @@ def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste, 
             plt.figure(figsize=(12, 6))
             plt.gca().set_facecolor('white')
 
+            # Separar os dados por flag
             df_preenchido = df_filtrado.copy()
             df_preenchido = df_preenchido.fillna(df_preenchido.mean())
             df_flag_0 = df_preenchido[df_preenchido[flag_column] != 4]
@@ -96,50 +68,78 @@ def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste, 
             df_nan = df_filtrado[df_filtrado[param].isna()]
             df_nan = df_nan.fillna(df_filtrado.mean())
             
+            # Contagem de dados por flags
             count_flag_0 = len(df_flag_0)
             count_flag_4 = len(df_flag_4)
             porcentagem_flag_0 = round((count_flag_0 / len(df_filtrado)) * 100, 2)
             porcentagem_flag_4 = round((count_flag_4 / len(df_filtrado)) * 100, 2)
             
+            # Plotar série completa
             plt.plot(df_filtrado['GMT-03:00'], df_filtrado[param], label=f'Série Completa: Flag 0 ({porcentagem_flag_0}%)', alpha=0.7, color='black', linestyle='-', linewidth=1, zorder=1)
+
+            # Plotar os dados com Flag 4 (vermelho) como pontos
             plt.scatter(df_flag_4['GMT-03:00'], df_flag_4[param], color='red', alpha=1, s=15, label=f'Flag 4 ({porcentagem_flag_4}%)', zorder=2)
-            plt.plot(df_nan['GMT-03:00'], df_nan[param], alpha=0.8, color='black', linestyle='dotted', linewidth=1, label='Valores nulos', zorder=3)
             
+            # Plotar os nan como a média da série temporal
+            plt.plot(df_nan['GMT-03:00'], df_nan[param], alpha=0.8, color='black', linestyle='dotted', linewidth=1, label = 'Valores nulos', zorder=3)
+
+            # Configurações do gráfico
             data_inicio = str(df_filtrado['GMT-03:00'].iloc[0])
             data_fim = str(df_filtrado['GMT-03:00'].iloc[-1])
             
             plt.title(f'Série Temporal: {parametro_para_teste} - {param} - execução do reporte: {agora}\nPeríodo de análise: {data_inicio} - {data_fim}')
             plt.xlabel('Tempo (Data/Hora)')
-            plt.ylabel(configurar_rotulos(param))
             
-            xticks_positions = configurar_ticks_x(df_filtrado)
+            # Definir o eixo Y conforme o tipo de parâmetro
+            if "Amplitude" in param:
+                plt.ylabel('Amplitude (m)')
+            if "Speed" in param:
+                plt.ylabel('Velocidade (m/s)')
+            if "Temperature" in param:
+                plt.ylabel('Temperatura (°C)')
+            if "Pressure" in param:
+                plt.ylabel('Pressão (dbar)')
+
+            # Garantir que o eixo X tenha sempre 5 ticks
+            time_min = df_filtrado['GMT-03:00'].min()
+            time_max = df_filtrado['GMT-03:00'].max()
+            time_min_timestamp = time_min.timestamp()
+            time_max_timestamp = time_max.timestamp()
+
+            xticks_positions_timestamp = np.linspace(time_min_timestamp, time_max_timestamp, 5)
+            xticks_positions = [pd.to_datetime(ts, unit='s') for ts in xticks_positions_timestamp]
             plt.xticks(xticks_positions, rotation=0)
 
+            # Definir limites do eixo Y
             if param in amplitude_cols:
                 plt.ylim(ymin_amplitude, ymax_amplitude)
-                plt.yticks(np.linspace(ymin_amplitude, ymax_amplitude, 6))  # Garantir 5 ticks
-            elif param in speed_cols:
+            if param in speed_cols:
                 plt.ylim(ymin_speed, ymax_speed)
-                plt.yticks(np.linspace(ymin_speed, ymax_speed, 6))  # Garantir 5 ticks
 
             plt.legend(loc='upper right')
             plt.grid(True, linestyle='dotted', alpha=0.5)
+            
+            # Salvar o gráfico para o Streamlit (não em um diretório)
+            img_stream = io.BytesIO()
+            plt.savefig(img_stream, format='png')
+            img_stream.seek(0)  # Rewind the image stream
 
-            os.makedirs(pasta_saida, exist_ok=True)
-
-            safe_param_name = param.replace(' ', '_').replace('(', '').replace(')', '').replace('#', '').replace('/', '_').replace('\\', '_').replace('*', '')
-            safe_initial_date_name = data_inicio.replace(' ', '_').replace('(', '').replace(')', '').replace('#', '').replace('/', '_').replace('\\', '_').replace('*', '').replace(':', '')
-            safe_final_date_name = data_fim.replace(' ', '_').replace('(', '').replace(')', '').replace('#', '').replace('/', '_').replace('\\', '_').replace('*', '').replace(':', '')
-
-            plt.savefig(os.path.join(pasta_saida, f'{parametro_para_teste} - Flag_{safe_param_name} - {safe_initial_date_name} - {safe_final_date_name}.png'))
+            # Utilizar o download_button para permitir o download da imagem gerada
+            file_name = f'{parametro_para_teste} - Flag_{param} - {data_inicio} - {data_fim}.png'
+            st.download_button(
+                label=f'Download do gráfico: {file_name}',
+                data=img_stream,
+                file_name=file_name,
+                mime="image/png"
+            )
             plt.close()
 
     print("\nREPORTE EM GRÁFICOS FINALIZADO.")
 
-#%% Executando a função para exibir a página no Streamlit
+# Função principal
 def main():
+    # Chamando a função do Streamlit para exibir a página
     exibir_pagina_streamlit()
 
-# Chama a função principal se o arquivo for executado diretamente
 if __name__ == "__main__":
     main()
