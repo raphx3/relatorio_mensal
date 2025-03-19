441edd88ceb7cab3c672c07bd03535b4af31d701
raphx3 <raphaelalvarengadasilva@gmail.com>
2025-03-19 17:38:31 +0000

    Update GERAR_RELATORIO_PD_METEO.py

--- GERAR_RELATORIO_PD_METEO.py
+++ GERAR_RELATORIO_PD_METEO.py
@@ -1,253 +1,11 @@
-#%% Modulos
-import os
-import pandas as pd
+import io
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-import datetime as dtt
 import streamlit as st
-import time
-#from QC_FLAGS_UMISAN_VER08_REVRA import *
-#from QC_OPERACIONAL_UMISAN_VER09_REVRA import * # é de onde vem a variavel df 
-from QC_FLAGS_UMISAN import *
-from OPERACIONAL_UMI_SIMPLIFICADO import *
+import datetime as dtt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
-#%% FRONT END STREAMLIT
-
-
-def exibir_pagina_streamlit():
-    st.title('Relatório mensal')
-    
-    # Campos para o usuário inserir informações de configuração de teste
-    analista = st.text_input('Nome do Analista:', 'Raphael')  # Nome do analista
-    projeto = st.text_input('Nome do Projeto:', 'PD_METEO')  # Nome do projeto
-    boia = st.text_input('Nome da Boia:', 'Protótipo 1')  # Nome da boia
-    #parametro_para_teste = st.text_input('Selecione o parâmetro para teste:')  # Nome da boia
-    
-    # Seleção do parâmetro para teste
-    parametro_para_teste = st.selectbox(
-        'Selecione o parâmetro para teste:',
-        ['CORRENTES', 'METEOROLOGIA', 'MARE', 'ONDAS', 'ONDAS_NAO_DIRECIONAIS']
-    )
-
-    # Usando st.columns() para criar colunas lado a lado
-    col1, col2 = st.columns(2)  # Cria duas colunas
-
-    # Campo para o usuário inserir as datas de início e fim lado a lado
-    with col1:
-        data_inicio = st.date_input('Data de início:', pd.to_datetime('2024-01-01'))
-    
-    with col2:
-        data_fim = st.date_input('Data de fim:', pd.to_datetime('2025-12-31'))
-
-    # Campo para o usuário inserir o caminho da pasta onde os resultados serão salvos
-    pasta_saida = st.text_input(
-        'Insira o caminho da pasta onde os resultados serão salvos:', 
-        r'C:\Users\Rafael Alvarenga UMI\Desktop\PD_METEO\REPORTES\RELATORIO_MENSAL\RESULTADOS',
-        key='input_pasta_saida'
-    )
-
-    # Botão para gerar os resultados e salvar na pasta
-    if st.button(label='Gerar Resultados para Relatório na Pasta Selecionada'):
-        if pasta_saida:
-            # Verifica se o diretório existe, se não, cria o diretório
-            try:
-                os.makedirs(pasta_saida, exist_ok=True)  # Cria o diretório se não existir
-                st.success(f"Pasta criada em: {pasta_saida}")
-            except Exception as e:
-                st.error(f"Erro ao criar a pasta: {e}")
-                return
-
-            # Exibir a barra de progresso
-            progress_bar = st.progress(0)
-            progress_text = st.empty()
-
-            # Filtrando dados por parametro
-            try:
-                # Primeira etapa: Filtrando os dados (0% - 25%)
-                progress_text.text("Filtrando dados...")
-                for i in range(1, 26):  # Vai de 0% até 25%
-                    progress_bar.progress(i)
-                    time.sleep(0.1)  # Espera 1 segundo a cada incremento                 
-                    
-                if 'CORRENTES' in parametro_para_teste:
-                    parameter_columns=parameter_columns_correntes
-                    #df_correntes,parameter_columns=importar_dados_corrente_string_ADCP(df_PNORC,df_PNORI,df_PNORS,parameter_columns_PNORC,parameter_columns_PNORI,parameter_columns_PNORS,parameter_columns)
-                    #df_correntes= aplicar_filtros(df_correntes,parameter_columns,dict_offset, limites_range_check, dict_max_min_test, st_time_series_dict, limite_repeticao_dados, limite_sigma_aceitavel_and_dict_delta_site, sampling_frequency, coluna_tempo, alert_window_size, dict_spike,dict_lt_time_and_regressao)
-                    df=df_correntes
-
-                
-                if 'METEOROLOGIA' in parametro_para_teste:
-                    parameter_columns=parameter_columns_meteo
-                    #df_meteo, nomes_colunas = import_df_meteo(input_file_meteo, nomes_colunas=parameter_columns_meteo)
-                    #df_meteo= aplicar_filtros(df_meteo,parameter_columns,dict_offset, limites_range_check, dict_max_min_test, st_time_series_dict, limite_repeticao_dados, limite_sigma_aceitavel_and_dict_delta_site, sampling_frequency, coluna_tempo, alert_window_size, dict_spike,dict_lt_time_and_regressao)
-                    df=df_meteo
-
-                
-                if 'MARE' in parametro_para_teste:
-                    parameter_columns=parameter_columns_mare
-                    #df_tide,nomes_colunas= import_df_mare(input_file_mare, nomes_colunas=parameter_columns_mare)
-                    #df_tide= aplicar_filtros(df_tide, parameter_columns, dict_offset, limites_range_check, dict_max_min_test, st_time_series_dict, limite_repeticao_dados, limite_sigma_aceitavel_and_dict_delta_site, sampling_frequency, coluna_tempo, alert_window_size, dict_spike,dict_lt_time_and_regressao)
-                    df=df_tide
-
-                
-                if 'ONDAS' in parametro_para_teste:
-                    parameter_columns=parameter_columns_ondas
-                    #df_ondas = process_wave_data(df_PNORW, df_PNORB, df_PNORI, df_PNORS,parameter_columns_PNORW, parameter_columns_PNORB, parameter_columns_PNORI, parameter_columns_PNORS, parameter_columns_ondas)
-                    #df_ondas= aplicar_filtros(df_ondas,parameter_columns,dict_offset, limites_range_check, dict_max_min_test, st_time_series_dict, limite_repeticao_dados, limite_sigma_aceitavel_and_dict_delta_site, sampling_frequency, coluna_tempo, alert_window_size, dict_spike,dict_lt_time_and_regressao)
-                    df=df_ondas
-
-                          
-                if 'ONDAS_NAO_DIRECIONAIS' in parametro_para_teste:
-                        parameter_columns=parameter_columns_ondas_nao_direcionais                    
-                        #df_ondas_nao_direcionais = pd.read_csv(input_file_ondas_nao_direcionais,header=1,sep=',',names=parameter_columns_ondas_nao_direcionais)
-                        #df_ondas_nao_direcionais.rename(columns={"TIMESTAMP": "GMT-03:00"}, inplace=True)
-                        #for coluna in df_ondas_nao_direcionais.columns:
-                            #df_ondas_nao_direcionais[f'Flag_{coluna}'] = 0
-                        #df_ondas_nao_direcionais,resultados=aplicar_filtros(df_ondas_nao_direcionais, parameter_columns, dict_offset, limites_range_check, dict_max_min_test, st_time_series_dict, limite_repeticao_dados, limite_sigma_aceitavel_and_dict_delta_site, sampling_frequency, coluna_tempo, alert_window_size, dict_spike, dict_lt_time_and_regressao)
-                        df=df_ondas_nao_direcionais
-                    
-                      
-                # Filtragem dos dados por tempo
-                df_filtrado_por_tempo, inicio, fim = filtrar_por_periodo(df, data_inicio, data_fim)
-            
-                # Segunda etapa: Processando dados (25% - 75%)
-                progress_text.text("Processando dados...")
-                for i in range(26, 76):  # Vai de 25% até 75%
-                    progress_bar.progress(i)
-                    time.sleep(0.1)  # Espera 1 segundo a cada incremento
-                
-             
-                
-                plot_series_temporais(df_filtrado_por_tempo, parameter_columns, parametro_para_teste, os.path.join(pasta_saida, parametro_para_teste))
-            
-                # Terceira etapa: Gerando o relatório (75% - 100%)
-                progress_text.text("Gerando o relatório...")
-                for i in range(76, 101):  # Vai de 75% até 100%
-                    progress_bar.progress(i)
-                    time.sleep(0.1)  # Espera 1 segundo a cada incremento
-                progress_text.text("Processo finalizado.")
-                
-                # Finalizando o processo
-                output_file = os.path.join(pasta_saida)
-                st.success(f"Relatório gerado com sucesso em: {output_file}\{parametro_para_teste}")
-            
-            except Exception as e:
-                st.error(f"Erro ao gerar os resultados: {e}")
-                progress_text.text("Erro durante o processo.")
-
-
-
-
-#%% filtrar_por_periodo
-
-def filtrar_por_periodo(df, data_inicio, data_fim):
-    
-    # Filtro
-    data_inicio = pd.to_datetime(data_inicio)
-    data_fim = pd.to_datetime(data_fim)
-    df_filtrado_por_tempo = df[(df['GMT-03:00'] >= data_inicio) & (df['GMT-03:00'] <= data_fim)]
-    
-    return df_filtrado_por_tempo, data_inicio.strftime("%Y-%m-%d %H:%M:%S"), data_fim.strftime("%Y-%m-%d %H:%M:%S")
-
-
-
-#%% Dicionários
-# Parametros de interesse dados maré
-parameter_columns_mare = [
-    'GMT-03:00', 
-    'Pressure_S1',
-    'Pressure_S2', 
-
-    ]
-# Parametros de interesse dados meteorologicos
-parameter_columns_meteo= [
-    'GMT-03:00',
-    'Pressure(hPa)', 
-    'Rain', 
-    'Wind Direction(*)',
-    'Gust Speed(m/s)',
-    'Wind Speed(m/s)', 
-    'Dew Point',
-    'RH(%)',
-    'Temperature(*C)'
-    ]
-
-# Parametros de interesse dados correntes
-parameter_columns_correntes=[
-    'GMT-03:00',
-    'Battery',
-    'Heading',
-    'Pitch',
-    'Roll',
-    'Pressure(dbar)',
-    'Temperature(C)',
-    ]
-
-# Parametros de interesse dados ondas
-parameter_columns_ondas = [
-    'GMT-03:00',  # Data e hora no fuso horário GMT-03:00 (horário de Brasília ou outro fuso horário local),
-    'Hm0',  # Altura significativa das ondas (m), média das maiores 1/3 das ondas em um período de tempo
-    'Hmax',  # Altura máxima das ondas (m), a maior altura registrada das ondas em um período de tempo
-    'Hm0_sea',  # Altura significativa das ondas no mar (m)
-    'Hm0_swell',  # Altura significativa das ondas na ondulação (m),
-    'Tm02',  # Período médio de onda (s), tempo médio entre duas ondas consecutivas
-    'Tp',  # Período de pico das ondas (s), o período associado à maior energia no espectro de ondas
-    'Tp_sea',  # Período de pico das ondas no mar (s)
-    'Tp_swell',  # Período de pico das ondas na ondulação (s),
-    'DirTp',  # Direção do período de pico das ondas (graus), direção de propagação das ondas associadas ao Tp
-    'DirTp_sea',  # Direção do período de pico das ondas no mar (graus)
-    'DirTp_swell',  # Direção do período de pico das ondas na ondulação (graus)
-    'Main Direction',  # Direção principal das ondas (graus), direção predominante das ondas
-    'Main Direction_sea',  # Direção principal das ondas no mar (graus)
-    'Main Direction_swell',  # Direção principal das ondas na ondulação (graus)
-    'Mean pressure',  # Pressão média (não especificada)
-    'Battery',  # Nível da bateria do sensor
-    'Heading',  # Rumo ou direção da embarcação/sensor (graus)
-    'Pitch',  # Inclinação do sensor em relação ao eixo horizontal (graus)
-    'Roll',  # Inclinação lateral do sensor (graus)
-    'Pressure(dbar)',  # Pressão medida (dbar)
-    'Temperature(C)',  # Temperatura da água (°C)
-]
-
-parameter_columns_ondas_nao_direcionais = [
-    'GMT-03:00',
-    'Tide_Level',
-    "Sensor_Velki", 
-    'CutOff_Freq_High',
-    'Peak_Period',
-    'Mean_Period',
-    'Max_Height',
-    'Sign_Height',
-
-    
-    ]
-
-func_names = [
-    "time_offset", 
-    "range_check_sensors", 
-    "range_check_environment", 
-    "identificar_gaps", 
-    "identificar_dados_nulos", 
-    "spike_test", 
-    "lt_time_series_rate_of_change", 
-    "teste_continuidade_tempo", 
-    "identificar_duplicatas_tempo", 
-    "verifica_dados_repetidos", 
-    "st_time_series_segment_shift", 
-    "max_min_test", 
-    "verificar_temperatura_vs_ponto_de_orvalho", 
-    "verificar_velocidade_vs_rajada", 
-    "verificar_altura_max_vs_sig", 
-    "gradiente_de_amplitude_do_sinal", 
-    "detectar_platos", 
-    "taxa_de_mudanca_vertical"
-]
-
-
-
-
-
-#%% plot_series_temporais
 def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste):
     agora = dtt.datetime.now()
     agora = agora.strftime('%Y/%m/%d %H:%M:%S')
@@ -312,16 +70,22 @@ def plot_series_temporais(df_filtrado, parameter_columns, parametro_para_teste):
 
     return buf
 
-#%% Executando a função para exibir a página no Streamlit
-
-
+# Função principal para exibir o Streamlit
 def main():
     st.title('Visualização de Séries Temporais')
     
     # Carregar os dados (df_filtrado, parameter_columns, etc.)
     # Aqui você deve ter o dataframe e os parâmetros configurados, vou usar um exemplo genérico para esse caso:
-    df_filtrado_por_tempo = pd.DataFrame()  # Substitua com seus dados reais
-    parameter_columns = ['Pressure_S1', 'Pressure_S2']  # Exemplos de colunas, substitua conforme necessário
+    
+    # Exemplo de dados (substitua com os seus dados reais)
+    df_filtrado_por_tempo = pd.DataFrame({
+        'GMT-03:00': pd.date_range(start='2023-01-01', periods=100, freq='D'),
+        'Amplitude_S1': np.random.rand(100) * 10,
+        'Speed_S1': np.random.rand(100) * 20,
+        'Flag_Amplitude_S1': np.random.choice([0, 4], size=100),
+        'Flag_Speed_S1': np.random.choice([0, 4], size=100),
+    })
+    parameter_columns = ['Amplitude_S1', 'Speed_S1']  # Exemplos de colunas
     parametro_para_teste = 'ExemploTeste'  # Nome do parâmetro de teste
     
     # Gerar o gráfico em memória
